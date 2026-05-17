0a02ea90547d05db8ece5e2262cc59b61f336c1d
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2026-05-17 20:28:47 +0200

    feat: create auth component + service

---
+++ src/app/auth/auth-form/auth-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-form',
+  imports: [],
+  templateUrl: './auth-form.html',
+  styleUrl: './auth-form.css',
+})
+export class AuthForm {}
